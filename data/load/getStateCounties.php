f19e3ddf3f8bf7eb66e3f858870ad7fba2f32adb
a <alex@persephone.(none)>
2012-12-17 12:33:39 -0500

    get State Counties

--- data/load/getStateCounties.php
+++ data/load/getStateCounties.php
@@ -7,24 +7,30 @@ $geo = json_decode($foo, true);
 
 $source = $_GET['s'];
 $var = $_GET['v'];
-
-
+$source = 0;
+$fips = $_GET['fip'];
+    
 $sources = Array('sf1','acs5');
-$handles = Array( 'sf1' =>Array('P0010001','P0030002','P0030003','P0030005','P0040001','P0120002','P0120026','P0180001'), acs5 => Array('B00001_001E','B00002_001E','B23001_001E','B25044_003E','B25044_004E','B25119_001E','B08006_008E','B08006_009E','B08006_011E'));
+$handles = Array( 'sf1' =>Array('P0010001','P0030002','P0030003','P0030005','P0040001','P0120002','P0120026','P0180001'), 'acs5' => Array('B00001_001E','B00002_001E','B23001_001E','B25044_003E','B25044_004E','B25119_001E','B08006_008E','B08006_009E','B08006_011E'));
 
 
 $cursor = 1;
 $geoSort=Array();
 foreach($geo['features'] as $feature){
-    $geoSort[$feature['properties']['GEO_ID']] = $feature;
+    $statefip = $feature['properties']['GEO_ID'][9].$feature['properties']['GEO_ID'][10];
+    if($statefip == $fips){
+        $geoSort[$feature['properties']['GEO_ID']] = $feature;
+    }
 }
 
-$validFIPS = Array(1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,45,46,47,48,49,50,51,53,54,55,56,72);
-
+//$validFIPS = Array(1,2,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,44,45,46,47,48,49,50,51,53,54,55,56,72);
+$validFIPS = Array($fips);
 
 foreach($validFIPS as $fipscode)
 {
-    $jURL = 'http://api.census.gov/data/2010/'.$sources[$source].'?key=564db01afc848ec153fa77408ed72cad68191211&get='.$handles[$sources[$source]][$var].'&for=county:*&in=state:'.$fipscode;
+    $vars = 'P0010001,P0030002,P0030003,P0030005,P0040001,P0120002,P0120026,P0180001';
+    $var = $handles[$sources[$source]][$var];
+    $jURL = 'http://api.census.gov/data/2010/'.$sources[$source].'?key=564db01afc848ec153fa77408ed72cad68191211&get='.$vars.'&for=county:*&in=state:'.$fipscode;
 
     //echo $jURL;
     $cdata = curl_download($jURL);
@@ -36,9 +42,15 @@ foreach($validFIPS as $fipscode)
 
     for($cursor =1;$cursor <count($cdata);$cursor++)
     {
-    $geoid = '0500000US'.$cdata[$cursor][1]. $cdata[$cursor][2];
+    $length = count($cdata[$cursor]);
+    //echo $length;
+    $geoid = '0500000US'.$cdata[$cursor][$length-2]. $cdata[$cursor][$length-1];
     //echo  $geoSort[$geoid]['properties']['NAME'].' '.$geoSort[$geoid]['properties']['LSAD'].' Population:'.number_format ($cdata[$cursor][0]).    "-$cursor".'<br>';
-        $geoSort[$geoid]['properties']['CDATA'] = $cdata[$cursor][0];
+        for($var = 0;$var < $length-2;$var++)
+        { 
+            //echo $handles[$sources[$source]][$var];
+         $geoSort[$geoid]['properties'][$handles[$sources[$source]][$var]] = $cdata[$cursor][$var];
+        }
         //echo  $cdata[$cursor][0];
     }
 }
